03d52c13af8be4539ad17266e4b2144bffd0dd67
sivader18 <sivader18@gmail.com>
2025-09-22 12:04:16 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+name="Mlproject",
+version='0.0.1',
+author="sivakumar",
+author_email="sivader18@gmail.com",
+packages=find_packages(),
+install_requires=["numpy","pandas","seaborn"]
+
+)
