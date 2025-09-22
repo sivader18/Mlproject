10362f5ca126ad29bbe3473e58e6570fded36994
sivader18 <sivader18@gmail.com>
2025-09-22 12:06:27 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(filepath:str)->List[str]:
+    """
+    this function is used to read the requirements.txt
+    """
+    requirements=[]
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
 
 
 setup(
@@ -7,6 +24,6 @@ version='0.0.1',
 author="sivakumar",
 author_email="sivader18@gmail.com",
 packages=find_packages(),
-install_requires=["numpy","pandas","seaborn"]
+install_requires=get_requirements('requirements.txt')
 
 )
